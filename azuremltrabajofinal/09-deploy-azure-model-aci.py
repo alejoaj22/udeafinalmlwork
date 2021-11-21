5b57188668b9e6a0f71612641e6a581797db7f9f
Andrés Soto <andres.soto.h@outlook.com>
2021-11-21 12:35:26 -0500

    fix error on libraries

--- azuremltrabajofinal/09-deploy-azure-model-aci.py
+++ azuremltrabajofinal/09-deploy-azure-model-aci.py
@@ -2,7 +2,7 @@ from azureml.core.model import InferenceConfig
 from azureml.core.environment import Environment
 from azureml.core import Workspace
 from azureml.core.model import Model
-from azureml.core.webservice import AciWebservice, AksWebservice
+from azureml.core.webservice import AciWebservice
 
 ws = Workspace.from_config(path='./.azureml',_file_name='config.json')
 model = Model(ws,name='house-price-model',version=3)
@@ -14,7 +14,7 @@ env = Environment.from_conda_specification(
 
 inference_config = InferenceConfig(entry_script="./src/score.py", environment=env)
 
-deployment_config = AksWebservice.deploy_configuration(cpu_cores = 1, memory_gb = 1)
+deployment_config = AciWebservice.deploy_configuration(cpu_cores = 1, memory_gb = 1)
 
 aci_service = Model.deploy(workspace=ws, 
                        name='house-price-service', 
