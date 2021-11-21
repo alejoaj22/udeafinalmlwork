89bad2c70b52a16c8c54de6bcf7b571790fca2c3
Andrés Soto <andres.soto.h@outlook.com>
2021-11-21 12:05:30 -0500

    edit scripts to deploy model

--- azuremltrabajofinal/src/score.py
+++ azuremltrabajofinal/src/score.py
@@ -3,20 +3,26 @@ import numpy as np
 import os
 import pickle
 import joblib
+from tensorflow import keras
 
 def init():
-    global model
+    global model, scaler
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It's the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION).
     # For multiple models, it points to the folder containing all deployed models (./azureml-models).
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'iris_model.pkl')
-    model = joblib.load(model_path)
+    scaler_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'scaler.pkl')
+    scaler = joblib.load(scaler_path)
+
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'my_model.h5')
+    model = keras.models.load_model(model_path)
 
 def run(raw_data):
+
     data = np.array(json.loads(raw_data)['data'])
+    scaled_data = scaler.transform(data)
+
     # Make prediction.
-    y_hat = model.predict(data)
-    # You can return any data type as long as it's JSON-serializable.
-    setosa_clases = ['Setosa', 'Versicolor', 'Virginica']
+    y_hat = model.predict(scaled_data)
+
     # return the result back
-    return json.dumps({"predicted_class": setosa_clases[int(y_hat)]})
+    return json.dumps({"predicted_price": float(y_hat)})
